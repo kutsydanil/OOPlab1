d49d9e18e887541a26fcabe6c67a315a2616f522
kutsydanil <kutsyy.danik@gmail.com>
2021-11-08 18:58:52 +0300

    OOPlab1
    OOPlab1

---
+++ lab1/Lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Lab1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static Trapezia newTrapecia()
+        {
+            Trapezia trapezia;
+            double a, b;
+            Console.WriteLine("Ввод значений переменной a и b");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите значение a(координату основания): ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введите значение b(координату основания): ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    trapezia = new Trapezia(a, b);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка ввода значения(неверный формат)!");
+                }
+            }
+            return trapezia;
+        }
+        static void Main(string[] args)
+        {
+            Trapezia trapezia = newTrapecia();
+            if (trapezia.Correct())
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    int N;
+                    Console.WriteLine("     Меню: ");
+                    Console.WriteLine("     1 - Подсчет значений сторон трапеции");
+                    Console.WriteLine("     2 - Подсчет периметра трапеции");
+                    Console.WriteLine("     3 - Подсчет площади трапеции");
+                    Console.WriteLine("     4 - Проверка принадлежности точки");
+                    Console.WriteLine("     5 - Создание новой трапеции");
+                    Console.WriteLine("     Выход");
+
+                    Console.WriteLine("Введите пункт меню: ");
+                    N = Convert.ToInt32(Console.ReadLine());
+                    switch (N)
+                    {
+                        case 1:
+                            int i = 1;
+                            Console.WriteLine("Длинны сторон трапеции: ");
+                            foreach (double side in trapezia.FindSidesLen())
+                            {
+                                Console.WriteLine("№" + Convert.ToString(i) + " = " + Convert.ToString(side));
+                                i = i + 1;
+                            }
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Периметр равен: " + trapezia.FindPerim());
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("Площадь равна: " + trapezia.FindSquare());
+                            break;
+                        case 4:
+                            double X, Y;
+                            Console.WriteLine("Ввод значений для проверки на принадлежность трапеции");
+                            Console.WriteLine("Введите значение X: ");
+                            X = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Введите значение Y:");
+                            Y = Convert.ToDouble(Console.ReadLine());
+                            if (trapezia.CheckPoint(X, Y))
+                            {
+                                Console.WriteLine("Точки принадлежат трапеции");
+                            }
+                            else Console.WriteLine("Точки не лежат в трапеции");
+                            break;
+                        case 5:
+                            Console.WriteLine("Создание новой трапеции");
+                            trapezia = newTrapecia();
+                            break;
+                        case 6:
+                            Console.WriteLine("Завершение работы");
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Нет такого пункта меню");
+                            break;
+                            
+                    }
+                }
+            }
+            else Console.WriteLine("Ввод некорректных данных");
+        }
+    }
+}
---
+++ lab1/Lab1/Lab1/Class1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Lab1
+{
+    public class Trapezia
+    {
+        /// <summary>
+        /// Основания трапеции
+        /// <param name="a"> Левая граница основания </param>
+        /// <param name="b"> Правая граница основания </param>
+        /// <summary>
+
+        private double a, b;
+
+        /// <summary>
+        /// Контсруктор класса Trapezia
+        /// <summary>
+        public Trapezia(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        /// <summary>
+        /// Проверка на корректность ввода a и b
+        /// <summary>
+        public bool Correct()
+        {
+            if ((a > 1 && b > a) || (b <= Math.Log(a)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Подсчет значения интеграла 
+        /// <summary>
+        private double FindIntegral()
+        {
+            double len = 0;
+            double dx = (b - a) / 1000;
+            double X = a;
+            double Xy = a + dx;
+            while (Xy < b)
+            {
+                len += Math.Sqrt(Math.Pow(Xy - X, 2) + Math.Pow(Math.Log(Xy) - Math.Log(X), 2));
+                X += dx;
+                Xy += dx;
+            }
+            return len;
+
+        }
+        /// <summary>
+        /// Подсчет длины сторон трапеции
+        /// <summary>
+        public double[] FindSidesLen()
+        {
+            double[] sides = new double[4];
+            sides[0] = b - a;
+            sides[1] = Math.Abs(Math.Cos(b));
+            sides[2] = FindIntegral();
+            sides[3] = Math.Abs(Math.Cos(a));
+            return sides;
+        }
+        /// <summary>
+        /// Нахождение площади трапеции
+        /// <summary>
+        public double FindSquare()
+        {
+            return (Math.Log(b) - Math.Log(a));
+        }
+        /// <summary>
+        /// Нахождение периметра трапеции
+        /// <summary>
+        public double FindPerim()
+        {
+            double per = 0;
+            foreach (double side in this.FindSidesLen())
+            {
+                per = per + side;
+            }
+            return per;
+        }
+        /// <summary>
+        /// Проверка точек на принадлежность
+        /// <summary>
+        public bool CheckPoint(double X, double Y)
+        {
+            if (X > b || X < a)
+            {
+                return false;
+            }
+            else if(Y < 0 || Y > Math.Log(X))
+            {
+                return false;
+            }
+            else return true;
+        }
+    }
+
+}
